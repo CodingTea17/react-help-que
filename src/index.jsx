4363af62b339f47b597893615adde062b481c975
Dawson M <dawson.mortenson@gmail.com>
2017-11-18 22:27:42 -0800

    refactor the help queue project to utilize webpack

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Help Queue</h1>
+    <h3>5d</h3>
+    <h3>Bob and Time</h3>
+    <p><em>The clock isn't updating</em></p>
+  </div>,
+  document.getElementById('react-app-root')
+);
