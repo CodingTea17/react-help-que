42d7fa2340c9f88a013971d3e1638ff8d1b08e2f
Dawson M <dawson.mortenson@gmail.com>
2017-11-20 20:04:12 -0800

    add a class-based component for requesting help by hiding/showing a form and update the ticketlist to loop through alist of existing tickets

---
+++ src/components/NewTicketControl.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+class NewTicketControl extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {formVisableOnPage: false};
+    this.handleDisplayingNewTicketForm = this.handleDisplayingNewTicketForm.bind(this); 
+  }
+
+  handleDisplayingNewTicketForm(event){
+    console.log("New ticket button was clicked!");
+    this.setState({formVisibleOnPage: true});
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleDisplayingNewTicketForm}>Request Halp!</button>
+    );
+  }
+}
+
+export default NewTicketControl;
--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -1,11 +1,35 @@
 import React from "react";
 import Ticket from "./Ticket"
+
+var masterTicketList = [
+  {
+    names: "Tim and Franz",
+    location: "3A",
+    issue: "Firebase won't save record. Halp."
+  },
+  {
+    names: "Javi and Keegan",
+    location: "4B",
+    issue: "Fox image not displaying on page, can only see duck?"
+  },
+  {
+    names: "Aaron & Marc",
+    location: "9F",
+    issue: "Donkey picture not displaying on hover in Zoology app. :("
+  }
+];
+
 function TicketList(props){
   return (
-    <Ticket
-      location="3a"
-      names="Bob and Tim"
-      issue="The clock isn't updating"/>
+    <div>
+      <hr/>
+      {masterTicketList.map((ticket, index) =>
+        <Ticket names={ticket.names}
+                location={ticket.location}
+                issue={ticket.issue}
+                key={index}/>
+      )}
+    </div> 
   );
 }
 
